38fd03dc88cd6bf8c9ae7f88d11f12a4250f189b
Licedej <mariyarybakova11@gmail.com>
2024-09-20 11:43:55 +0300

    Add files via upload

---
+++ lesson_1.py
@@ -0,0 +1,16 @@
+import math
+
+
+r = float(input())
+print(round(math.pi*r*2, 2))
+print(round(math.pi*r**2, 2))
+
+
+x, y = 10, 55
+print(x, y)
+x, y = y, x
+print(x, y)
+
+
+l = float(input())
+print(round(2*math.pi*((l/9.81)**0.5), 2))

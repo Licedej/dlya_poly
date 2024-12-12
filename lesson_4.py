04e1cd10128fa96fe72d153a0bee3e2d15928b74
Lemma Licova <mariyarybakova11@gmail.com>
2024-12-12 23:37:04 +0300

    rep

--- lesson_4.py
+++ lesson_4.py
@@ -1,17 +1,15 @@
 # № 1
 
 k, i = 0, 0
-n, m, l = [], [], []
+n, l = [], []
 a = input()
-b = a + 'н'
+b = a + '+н'
 while i < len(b):
     l.append(b.index('н', i,))
     i += 1
-l = set(l)
-for i in l:
-    m.append(i)
-for i in range(len(m) - 1):
-    if m[i + 1] == m[i] + 1:
+l = list(set(l))
+for i in range(len(l) - 1):
+    if l[i + 1] == l[i] + 1:
         k += 1
     else:
         n.append(k + 1)
@@ -22,21 +20,20 @@ print(max(n))
 
 # № 2
 
-def func(text, b=0, i=0):
-    if text[i] == '(':
-        b = i + 1
-    elif text[i] == ')':
-        return slice(b, i)
-    return func(text, b, i + 1)
- 
+def func(text):
+    if '(' and ')' in text:
+        i = text.index('(')
+        while text[i] != text[text.index(')')]:
+            i += 1
+        return slice(text.index('(')+1, i)
 text = input()
 print(text[func(text)])
+
 
 
 # № 3
 
 str = input()
- 
 for w in str.split(): 
     if(w.startswith("а") and w.endswith("я")): 
         print(w) 

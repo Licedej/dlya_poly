b939f83d8d97cfda76b0606820266f958677e812
Lemma Licova <mariyarybakova11@gmail.com>
2024-12-12 22:46:42 +0300

    dop

--- lesson_3.py
+++ lesson_3.py
@@ -12,3 +12,15 @@ print(l)
 n = 9
 for i in range(1,n+1):
     print(*("{:^3}".format(i*j) for j in range(1, n+1)))
+
+# № 2*
+
+for i in range(9, 0, -1):
+    lin = [i*j for j in range(1, 10)]
+    for j in range(len(lin)):
+        if len(str(lin[j])) == 1 and j != 8:
+             print(f'{lin[j]}  ', end='')
+        elif len(str(lin[j])) != 1 and j != 8:
+             print(f'{lin[j]} ', end='')
+        else:
+             print(f'{lin[j]}', end='\n')
